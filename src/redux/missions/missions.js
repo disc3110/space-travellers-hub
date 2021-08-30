b7d51b1210b033537759f87bfe6fb1222908f2d8
Sebastián M <smunozmo@gmail.com>
2021-08-30 16:54:48 -0500

    Fix linters.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,6 +1,4 @@
-
 const SUCCESS_MISSION = 'SUCCESS_MISSION';
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/JRvgmQWxBSuTgawpguvn/books/';
 
 const initialState = [];
 
@@ -11,12 +9,9 @@ export const successMission = (payload) => ({
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case SUCCESS_BOOK: {
+    case SUCCESS_MISSION: {
       return Object.keys(action.payload).map((key) => ({
         id: key,
-        title: action.payload[key][0].title,
-        author: uniqueNamesGenerator({ dictionaries: [starWars] }),
-        genre: action.payload[key][0].category,
       }));
     }
     default:
