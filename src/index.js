8974f5c1a8e61f674bc9edf438869a2a08cc2049
Diego Solis <disc3110@gmail.com>
2021-08-31 09:18:14 -0500

    fetch-rockets

--- src/index.js
+++ src/index.js
@@ -2,30 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Header from './components/header';
 import Misssions from './pages/Missions';
 import Profile from './pages/Profile';
-import Rockets from './pages/Rockets';
-import getRockets from './API/rockets';
-
-getRockets();
+import RocketsPage from './pages/RocketsPage';
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path="/missions">
-          <Misssions />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-        <Route exact path="/">
-          <Rockets />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/missions">
+            <Misssions />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/">
+            <RocketsPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/redux/api/rockets.js
+++ src/redux/api/rockets.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 
 const baseURL = 'https://api.spacexdata.com/v3/rockets';
@@ -5,7 +6,7 @@ const baseURL = 'https://api.spacexdata.com/v3/rockets';
 const getRockets = async () => {
   try {
     const response = await axios.get(baseURL);
-    console.log(response.data);
+    return response.data;
   } catch (error) {
     // eslint-disable-next-line no-console
     console.log(error);
