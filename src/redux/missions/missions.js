8c48f1eb38e0e75fb521088f31cb2e7b8040135f
Sebastián M <smunozmo@gmail.com>
2021-08-31 11:56:10 -0500

    Add join mission action.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,9 +1,15 @@
 const SUCCESS_MISSION = 'SUCCESS_MISSION';
+const JOIN_MISSION = 'JOIN_MISSION';
 
 const initialState = [];
 
 export const successMission = (payload) => ({
   type: SUCCESS_MISSION,
+  payload,
+});
+
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
   payload,
 });
 
@@ -14,7 +20,17 @@ const reducer = (state = initialState, action) => {
         mission_id: element.mission_id,
         mission_name: element.mission_name,
         description: element.description,
+        member: false,
+        mission_button: 'Join Mission',
       }));
+    }
+    case JOIN_MISSION: {
+      const newState = state.map(mission => {
+        if(mission.mission_id !== action.payload.id)
+            return mission;
+        return { ...mission, member: true };
+    });
+    return newState;
     }
     default:
       return state;
