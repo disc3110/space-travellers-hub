0ad11486293deb7a69ce3d1a9b62920bd031b94d
Sebastián M <smunozmo@gmail.com>
2021-08-31 09:49:09 -0500

    Fix linters.

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,4 +1,3 @@
-/* disable-eslint */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { successMission } from '../redux/missions/missions';
